3c9f23fce867f8eb615ff2435aaa8a723e6c618d
esmecs <2853@holbertonschool.com>
2021-02-08 01:09:22 +0000

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int s;
+for (s = 48; s <= 57; s++)
+{
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
