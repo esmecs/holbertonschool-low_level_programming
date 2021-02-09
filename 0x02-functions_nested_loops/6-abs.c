dadaf4399cf1d22a08a6684086a0b3be520bcfb1
esmecs <2853@holbertonschool.com>
2021-02-09 22:40:41 +0000

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - return the absolute value
+ *
+ * @x:value
+ * Return: To the value x
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+x = x * -1;
+return (x);
+}
+else
+{
+return (x);
+}
+}
