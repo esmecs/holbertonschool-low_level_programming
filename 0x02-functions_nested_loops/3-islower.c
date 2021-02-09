4a248e0f53937d609548aaeb273a63e998ed18aa
esmecs <2853@holbertonschool.com>
2021-02-09 17:27:38 +0000

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+# include "holberton.h"
+/**
+ * _islower - return value
+ *
+ * @c: the first character to be evalue
+ * Return: value
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
