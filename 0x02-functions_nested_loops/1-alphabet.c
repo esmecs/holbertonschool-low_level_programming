4e3891cb128478004f3da594b7256cae98e754a6
esmecs <2853@holbertonschool.com>
2021-02-09 16:13:48 +0000

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Retur: void
+ */
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
